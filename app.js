31ea674a61e021e7bd79d4f0672471c20459c448
Kasapciuc Daniil <kasapciucdaniil@gmail.com>
2021-03-24 14:39:38 +0200

    init

--- app.js
+++ app.js
@@ -6,20 +6,16 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json({ extended: true }))
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/note', require('./routes/note.routes'))
 
-const PORT = config.get('port') || 5000
-
-//здесь наше приложение отдаёт статику
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-//обслуживание html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const PORT = config.get('port') || 5000
 
 async function start() {
   try {
